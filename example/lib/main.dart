757c1d6e3053ac4c3f8c908f35e5c0e3685c9d7a
Abdusin <Abdusin65@gmail.com>
2021-11-22 16:24:18 +0300

    Example Updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,23 +36,42 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var value = 0.0;
+  var value = 50.0;
   void onChanged(double _value) => setState(() => value = _value);
+  var value2 = 40.0;
+  void onChanged2(double _value) => setState(() => value2 = _value);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
         SizedBox(height: 20),
-        Container(
-          height: 230,
-          child: VerticalSlider(
-            onChanged: onChanged,
-            max: 100,
-            min: 0,
-            value: value,
-            width: 80,
+        Row(children: [
+          Spacer(),
+          Container(
+            height: 230,
+            child: VerticalSlider(
+              onChanged: onChanged,
+              max: 100,
+              min: 0,
+              value: value,
+              width: 80,
+            ),
           ),
-        ),
+          Spacer(),
+          Container(
+            height: 230,
+            child: VerticalSlider(
+              onChanged: onChanged2,
+              max: 100,
+              min: 0,
+              value: value2,
+              width: 80,
+              activeTrackColor: Colors.amber,
+              inactiveTrackColor: Colors.amberAccent.withOpacity(0.5),
+            ),
+          ),
+          Spacer(),
+        ]),
       ]),
     );
   }
